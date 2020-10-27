b07a3f9c9ad5076257bc1fd39eb6901e999b4b20
Sascha Brunner <sbrunner@esri.com>
2020-10-27 23:29:22 +0100

    Add console output for SLIDE

--- webscene-slide-show/main.ts
+++ webscene-slide-show/main.ts
@@ -16,81 +16,85 @@ if (urlParams.has("webscene")) {
 }
 
 if (!urlParams.has("noinfo")) {
-  document.getElementById("titleDiv")!.style.display = "block";
-  document.getElementById("slideTitleDiv")!.style.display = "block";
-  document.getElementById("slideCountDiv")!.style.display = "block";
-  document.getElementById("progressbarDiv")!.style.display = "block";
+    document.getElementById("titleDiv")!.style.display = "block";
+    document.getElementById("slideTitleDiv")!.style.display = "block";
+    document.getElementById("slideCountDiv")!.style.display = "block";
+    document.getElementById("progressbarDiv")!.style.display = "block";
 }
 
 let startSlide = 0;
 if (urlParams.has("start")) {
-  startSlide = parseInt(urlParams.get("start") || startSlide.toString(), 0);
+    startSlide = parseInt(urlParams.get("start") || startSlide.toString(), 0);
 }
 
 let animationTime = 3000;
 if (urlParams.has("animation")) {
-  animationTime = parseInt(urlParams.get("animation") || animationTime.toString(), 0);
+    animationTime = parseInt(urlParams.get("animation") || animationTime.toString(), 0);
 }
 
 let waitTime = 1000;
 if (urlParams.has("wait")) {
-  waitTime = parseInt(urlParams.get("wait") || waitTime.toString(), 0);
+    waitTime = parseInt(urlParams.get("wait") || waitTime.toString(), 0);
 }
 
 const webScene = new WebScene({
-  portalItem: {
-    id: webSceneId
-  }
+    portalItem: {
+        id: webSceneId
+    }
 });
 
 const view = new SceneView({
-  map: webScene,
-  container: "viewDiv",
-  ui: { components: [] } // disable the UI comonents
+    map: webScene,
+    container: "viewDiv",
+    ui: { components: [] } // disable the UI components
 });
 
 webScene.when(function () {
-  let slides = webScene.presentation.slides;
-  document.getElementById("titleDiv")!.innerHTML = webScene.portalItem.title;
-  document.getElementById("slideCountDiv")!.innerHTML = startSlide + "/" + slides.length;
+    let slides = webScene.presentation.slides;
+    document.getElementById("titleDiv")!.innerHTML = webScene.portalItem.title;
+    document.getElementById("slideCountDiv")!.innerHTML = startSlide + "/" + slides.length;
 
-  view.when(function () {
-    if (startSlide > 0) {
-      view.goTo(slides.getItemAt(startSlide - 1).viewpoint.camera);
-      document.getElementById("slideTitleDiv")!.innerHTML = slides.getItemAt(
-        startSlide - 1
-      ).title.text  || "";
-    }
+    view.when(function () {
+        if (startSlide > 0) {
+            view.goTo(slides.getItemAt(startSlide - 1).viewpoint.camera);
+            document.getElementById("slideTitleDiv")!.innerHTML = slides.getItemAt(
+                startSlide - 1
+            ).title.text || "";
+        }
 
-    setTimeout(updateLoading, 1000);
-    watchUtils.whenFalseOnce(view, "updating", function () {
-      goToSlide(startSlide);
-    });
-  });
-  function goToSlide(i: number) {
-    if (i < slides.length) {
-      document.getElementById("slideCountDiv")!.innerHTML = i + 1 + "/" + slides.length;
-      let slide = slides.getItemAt(i);
-      document.getElementById("slideTitleDiv")!.innerHTML = slide.title.text || "";
-
-      slide
-        .applyTo(view, {
-          duration: animationTime
-        })
-        .then(function () {
-          watchUtils.whenFalseOnce(view, "updating", function () {
+        setTimeout(updateLoading, 1000);
+        watchUtils.whenFalseOnce(view, "updating", function () {
+            console.log("SLIDE_" + startSlide + "_" + slides.length);
             window.setTimeout(function () {
-              goToSlide(i + 1);
+                goToSlide(startSlide);
             }, waitTime);
-          });
         });
-    } else {
-      console.log("STOP");
+    });
+    function goToSlide(i: number) {
+        if (i < slides.length) {
+            document.getElementById("slideCountDiv")!.innerHTML = i + 1 + "/" + slides.length;
+            let slide = slides.getItemAt(i);
+            document.getElementById("slideTitleDiv")!.innerHTML = slide.title.text || "";
+
+            slide
+                .applyTo(view, {
+                    duration: animationTime
+                })
+                .then(function () {
+                    watchUtils.whenFalseOnce(view, "updating", function () {
+                        console.log("SLIDE_" + (i + 1) + "_" + slides.length);
+                        window.setTimeout(function () {
+                            goToSlide(i + 1);
+                        }, waitTime);
+                    });
+                });
+        } else {
+            console.log("STOP");
+        }
     }
-  }
-  function updateLoading() {
-    let progressbarDiv = document.getElementById("progressbarDiv")!;
-    progressbarDiv.style.width = Math.min(10 * (view.performanceInfo as any).load, 100) + "%";
-    setTimeout(updateLoading, 1000);
-  }
+    function updateLoading() {
+        let progressbarDiv = document.getElementById("progressbarDiv")!;
+        progressbarDiv.style.width = Math.min(10 * (view.performanceInfo as any).load, 100) + "%";
+        setTimeout(updateLoading, 1000);
+    }
 });
